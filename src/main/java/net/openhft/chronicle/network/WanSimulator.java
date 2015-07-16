2ae28bd74def24b71e04133246c190ed35fbdf3e
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-07-16 19:15:47 +0100

    Add a wan simulator to reproduce timing sensitive issues with a random 0.5 ms delay.

--- src/main/java/net/openhft/chronicle/network/WanSimulator.java
+++ src/main/java/net/openhft/chronicle/network/WanSimulator.java
@@ -2,6 +2,8 @@ package net.openhft.chronicle.network;
 
 import net.openhft.chronicle.core.Jvm;
 
+import java.util.Random;
+
 /**
  * Created by peter.lawrey on 16/07/2015.
  */
@@ -9,11 +11,12 @@ public enum WanSimulator {
     ;
     private static final int NET_BANDWIDTH = Integer.getInteger("wanMB", 0);
     private static final int BYTES_PER_MS = NET_BANDWIDTH * 1000;
+    private static final Random RANDOM = new Random();
     private static long totalRead = 0;
 
     public static void dataRead(int bytes) {
         if (NET_BANDWIDTH <= 0) return;
-        totalRead += bytes + 128;
+        totalRead += bytes + RANDOM.nextInt(BYTES_PER_MS);
         int delay = (int) (totalRead / BYTES_PER_MS);
         if (delay > 0) {
             Jvm.pause(delay);
