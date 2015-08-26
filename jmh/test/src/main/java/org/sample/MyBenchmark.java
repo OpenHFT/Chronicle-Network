9a8c63408395f287af8b3d5ee2d6bac109dd93a4
rob <rob.austin@boundedbuffer.com>
2015-08-26 16:24:00 +0100

    added JMH

--- jmh/test/src/main/java/org/sample/MyBenchmark.java
+++ jmh/test/src/main/java/org/sample/MyBenchmark.java
@@ -1,29 +1,58 @@
-package net.openhft.performance.tests.network;
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.sample;
 
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.network.AcceptorEventHandler;
 import net.openhft.chronicle.network.TCPRegistry;
 import net.openhft.chronicle.network.VanillaSessionDetails;
-import net.openhft.chronicle.network.jmh.WireEchoRequestHandler;
 import net.openhft.chronicle.network.connection.TcpChannelHub;
+import net.openhft.chronicle.network.jmh.WireEchoRequestHandler;
 import net.openhft.chronicle.threads.EventGroup;
-import net.openhft.chronicle.wire.*;
+import net.openhft.chronicle.wire.TextWire;
+import net.openhft.chronicle.wire.Wire;
+import net.openhft.chronicle.wire.WireType;
 import org.jetbrains.annotations.NotNull;
-import org.junit.Assert;
-import org.junit.Test;
+import org.openjdk.jmh.annotations.Benchmark;
 
 import java.io.IOException;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.TimeUnit;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static net.openhft.chronicle.network.connection.SocketAddressSupplier.uri;
 
-/**
- * Created by rob on 26/08/2015.
- */
-public class SimpleServerAndClientTest {
+public class MyBenchmark {
 
-    @Test
+    @Benchmark
     public void test() throws IOException {
         String desc = "host.port";
         TCPRegistry.createServerSocketChannelFor(desc);
@@ -45,12 +74,11 @@ public class SimpleServerAndClientTest {
             tcpChannelHub.lock(() -> tcpChannelHub.writeSocket(wire));
 
             // read the reply from the socket ( timeout after 1 second )
-            Wire reply = tcpChannelHub.proxyReply(SECONDS.toMillis(1), tid);
+            Wire reply = tcpChannelHub.proxyReply(TimeUnit.SECONDS.toMillis(1), tid);
 
             // read the reply and check the result
             reply.readDocument(null, data -> {
                 final String text = data.read(() -> "payloadResponse").text();
-                Assert.assertEquals(expectedMessage, text);
             });
 
             eg.stop();
@@ -72,5 +100,4 @@ public class SimpleServerAndClientTest {
         SocketChannel sc = TCPRegistry.createSocketChannel(desc);
         sc.configureBlocking(false);
     }
-
 }
---
+++ src/main/java/net/openhft/chronicle/network/jmh/WireEchoRequestHandler.java
@@ -0,0 +1,44 @@
+package net.openhft.chronicle.network.jmh;
+
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.network.WireTcpHandler;
+import net.openhft.chronicle.network.api.session.SessionDetailsProvider;
+import net.openhft.chronicle.wire.Wire;
+import net.openhft.chronicle.wire.WireIn;
+import net.openhft.chronicle.wire.WireOut;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
+
+/**
+ * Created by rob on 26/08/2015.
+ */
+public class WireEchoRequestHandler extends WireTcpHandler {
+
+
+    public WireEchoRequestHandler(@NotNull Function<Bytes, Wire> bytesToWire) {
+        super(bytesToWire);
+    }
+
+
+    /**
+     * simply reads the csp,tid and payload and sends back the tid and payload
+     *
+     * @param inWire  the wire from the client
+     * @param outWire the wire to be sent back to the server
+     * @param sd      details about this session
+     */
+    @Override
+    protected void process(@NotNull WireIn inWire,
+                           @NotNull WireOut outWire,
+                           @NotNull SessionDetailsProvider sd) {
+
+        inWire.readDocument(m -> {
+            outWire.writeDocument(true, meta -> meta.write(() -> "tid")
+                    .int64(inWire.read(() -> "tid").int64()));
+        }, d -> {
+            outWire.writeDocument(false, data -> data.write(() -> "payloadResponse")
+                    .text(inWire.read(() -> "payload").text()));
+        });
+    }
+}
