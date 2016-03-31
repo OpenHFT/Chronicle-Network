b79e738b9829ebb2f6d5676a3aa4197dfb737962
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-03-31 16:24:15 +0100

    re-established tests as they are now working

--- src/main/java/net/openhft/chronicle/network/cluster/HostConnector.java
+++ src/main/java/net/openhft/chronicle/network/cluster/HostConnector.java
@@ -71,8 +71,8 @@ public class HostConnector implements Closeable {
 
         if (nc.socketChannel() != null)
             Closeable.closeQuietly(nc.socketChannel());
-
-        wp.close();
+        if (wp != null)
+            wp.close();
     }
 
 
