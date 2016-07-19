f40e03cd6293d3ded4bcc5c363e2af634d316cfa
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-07-19 12:08:31 +0100

    delta wire delta's on numerics was causing an issue so has been comment out.

--- src/main/java/net/openhft/chronicle/network/WireTcpHandler.java
+++ src/main/java/net/openhft/chronicle/network/WireTcpHandler.java
@@ -85,10 +85,8 @@ public abstract class WireTcpHandler<T extends NetworkContext>
     }
 
     public void wireType(@NotNull WireType wireType) {
-        if (wireType == BINARY) {
-            wireType = DELTA_BINARY.isAvailable() ? BINARY : BINARY;
-            // wireType = DELTA_BINARY.isAvailable() ? DELTA_BINARY : BINARY;
-        }
+        if (wireType == BINARY)
+            wireType = DELTA_BINARY.isAvailable() ? DELTA_BINARY : BINARY;
         this.wireType = wireType;
         if (publisher != null)
             publisher.wireType(wireType);
