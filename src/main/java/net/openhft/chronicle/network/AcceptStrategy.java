3d8e4537d0fabedb73d2232cb5345e1b0c230168
Jerry Shea <jerry.shea@chronicle.software>
2018-08-31 16:35:09 +1000

    doco AcceptStrategy

--- src/main/java/net/openhft/chronicle/network/AcceptStrategy.java
+++ src/main/java/net/openhft/chronicle/network/AcceptStrategy.java
@@ -9,13 +9,17 @@ import java.nio.channels.SocketChannel;
 /**
  * Can be used to reject incoming connections e.g. you could implement an AcceptStrategy that checks remote IPs
  * and rejects if not in whitelist
- *
- * Created by Jerry Shea on 29/08/18.
  */
 @FunctionalInterface
 public interface AcceptStrategy extends Marshallable {
 
     AcceptStrategy ACCEPT_ALL = ssc -> ssc.accept();
 
+    /**
+     * Determine whether to accept the incoming connection
+     * @param ssc
+     * @return null to reject the connection, otherwise return the accepted SocketChannel
+     * @throws IOException
+     */
     SocketChannel accept(ServerSocketChannel ssc) throws IOException;
 }
