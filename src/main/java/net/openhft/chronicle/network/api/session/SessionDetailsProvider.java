a1bbfc39db7cae374c40c66a7607885320ae8e29
Dmitry Pisklov <dpisklov@gmail.com>
2020-05-26 18:22:17 +0100

    Update copyrights

--- src/main/java/net/openhft/chronicle/network/api/session/SessionDetailsProvider.java
+++ src/main/java/net/openhft/chronicle/network/api/session/SessionDetailsProvider.java
@@ -1,11 +1,13 @@
 /*
- * Copyright 2016 higherfrequencytrading.com
+ * Copyright 2016-2020 Chronicle Software
+ *
+ * https://chronicle.software
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.openhft.chronicle.network.api.session;
 
 import net.openhft.chronicle.core.io.IORuntimeException;
@@ -29,10 +30,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.net.InetSocketAddress;
 import java.util.UUID;
-
-/*
- * Created by Rob Austin
- */
 public interface SessionDetailsProvider extends SessionDetails, Marshallable {
 
     void connectTimeMS(long connectTimeMS);
@@ -75,5 +72,5 @@ public interface SessionDetailsProvider extends SessionDetails, Marshallable {
     static SessionDetailsProvider create() {
         return new VanillaSessionDetails();
     }
-    
+
 }
