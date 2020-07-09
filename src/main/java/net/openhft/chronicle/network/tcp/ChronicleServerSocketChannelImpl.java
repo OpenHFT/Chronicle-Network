4e7a55def8356ac8759fb6fde602a350257ca1bb
Rob Austin <rob.austin@boundedbuffer.co.uk>
2020-07-09 11:09:20 +0100

    improvements in extracting the network layer into interfaces

---
+++ src/main/java/net/openhft/chronicle/network/tcp/ChronicleServerSocketChannelImpl.java
@@ -0,0 +1,90 @@
+package net.openhft.chronicle.network.tcp;
+
+import net.openhft.chronicle.core.io.AbstractCloseable;
+import net.openhft.chronicle.core.io.Closeable;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.net.SocketOption;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class ChronicleServerSocketChannelImpl extends AbstractCloseable implements ChronicleServerSocketChannel {
+
+    ServerSocketChannel ssc;
+
+    {
+        try {
+            ssc = ServerSocketChannel.open();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public ChronicleSocketChannel accept() throws IOException {
+        ssc.configureBlocking(true);
+        SocketChannel accept = ssc.accept();
+        return ChronicleSocketChannelFactory.wrap(accept);
+    }
+
+    @Override
+    public boolean isOpen() {
+        return ssc.isOpen();
+    }
+
+    @Override
+    public ChronicleServerSocket socket() {
+
+        return new ChronicleServerSocket() {
+
+            @Override
+            public int getLocalPort() {
+                return ssc.socket().getLocalPort();
+            }
+
+            @Override
+            public void close() {
+                Closeable.closeQuietly(ssc.socket());
+            }
+
+            @Override
+            public void setReuseAddress(final boolean b) throws SocketException {
+                ssc.socket().setReuseAddress(b);
+            }
+
+            @Override
+            public SocketAddress getLocalSocketAddress() {
+                return ssc.socket().getLocalSocketAddress();
+            }
+        };
+    }
+
+    @Override
+    protected void performClose() {
+        Closeable.closeQuietly(ssc);
+    }
+
+    @Override
+    public void bind(final InetSocketAddress address) throws IOException {
+        ssc.bind(address);
+    }
+
+    @Override
+    public SocketAddress getLocalAddress() throws IOException {
+        return ssc.getLocalAddress();
+    }
+
+    @Override
+    public void setOption(final SocketOption<Boolean> soReuseaddr, final boolean b) throws IOException {
+        ssc.setOption(soReuseaddr, b);
+    }
+
+    @Override
+    public void configureBlocking(final boolean configureBlocking) throws IOException {
+        ssc.configureBlocking(configureBlocking);
+    }
+
+};
--- src/main/java/net/openhft/chronicle/network/tcp/ChronicleSocketChannelFactory.java
+++ src/main/java/net/openhft/chronicle/network/tcp/ChronicleSocketChannelFactory.java
@@ -15,111 +15,16 @@ public enum ChronicleSocketChannelFactory {
     ;
 
     public static ChronicleSocketChannel wrap() throws IOException {
-        return newInstance(SocketChannel.open());
+        return wrap(SocketChannel.open());
     }
 
     public static ChronicleSocketChannel wrap(InetSocketAddress socketAddress) throws IOException {
-        return newInstance(SocketChannel.open(socketAddress));
+        return wrap(SocketChannel.open(socketAddress));
     }
 
     public static ChronicleSocketChannel wrap(@NotNull final SocketChannel sc) {
-        return newInstance(sc);
+        return new ChronicleSocketChannelImpl(sc);
     }
 
-    @NotNull
-    private static ChronicleSocketChannel newInstance(@NotNull final SocketChannel sc) {
-        return new ChronicleSocketChannel() {
-            @Override
-            public boolean isClosed() {
-                throw new UnsupportedOperationException("todo");
-            }
-
-            @Override
-            public void close() {
-                Closeable.closeQuietly(sc);
-            }
-
-            @Override
-            public @NotNull ChronicleSocketChannel socketChannel() {
-                return ChronicleSocketChannelFactory.newInstance(sc);
-            }
-
-            @Override
-            public int read(final ByteBuffer byteBuffer) throws IOException {
-                return sc.read(byteBuffer);
-            }
-
-            @Override
-            public int write(final ByteBuffer byteBuffer) throws IOException {
-                return sc.write(byteBuffer);
-            }
-
-            @Override
-            public long write(final ByteBuffer[] byteBuffers) throws IOException {
-                return sc.write(byteBuffers);
-            }
-
-            @Override
-            public void configureBlocking(final boolean blocking) throws IOException {
-                SocketChannel sc1 = sc;
-                if (sc1 != null)
-                    sc1.configureBlocking(blocking);
-            }
-
-            @Override
-            public InetSocketAddress getLocalAddress() throws IOException {
-                return (InetSocketAddress) sc.getLocalAddress();
-            }
-
-            @Override
-            public InetSocketAddress getRemoteAddress() throws IOException {
-                return (InetSocketAddress) sc.getRemoteAddress();
-            }
-
-            @Override
-            public boolean isOpen() {
-                return sc.isOpen();
-            }
-
-            @Override
-            public boolean isBlocking() {
-                return sc.isBlocking();
-            }
-
-            @Override
-            public ChronicleSocket socket() {
-                SocketChannel sc1 = sc;
-                if (sc1 == null)
-                    return null;
-                return ChronicleSocketFactory.toChronicleSocket(sc1.socket());
-            }
-
-            @Override
-            public void connect(final InetSocketAddress socketAddress) throws IOException {
-                sc.connect(socketAddress);
-            }
-
-            @Override
-            public void register(final Selector selector, final int opConnect) throws ClosedChannelException {
-                sc.register(selector, opConnect);
-            }
-
-            @Override
-            public boolean finishConnect() throws IOException {
-                return sc.finishConnect();
-            }
-
-            @Override
-            public void setOption(final SocketOption<Boolean> soReuseaddr, final boolean b) throws IOException {
-                sc.setOption(soReuseaddr, b);
-            }
-
-            @Override
-            public ISocketChannel toISocketChannel() {
-                return ISocketChannel.wrap(socketChannel());
-            }
-
-        };
-    }
 
 }
