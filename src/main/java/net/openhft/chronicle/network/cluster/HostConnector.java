7432515ba7cf698fbeb069a48008c7a08c3cd0b7
Jerry Shea <jerry.shea@chronicle.software>
2020-12-02 10:09:59 +1100

    compile error - temporary revert
    Signed-off-by: Jerry Shea <jerry.shea@chronicle.software>

--- src/main/java/net/openhft/chronicle/network/cluster/HostConnector.java
+++ src/main/java/net/openhft/chronicle/network/cluster/HostConnector.java
@@ -31,15 +31,9 @@ import java.io.Closeable;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
-import static java.io.Closeable.*;
 import static net.openhft.chronicle.core.io.Closeable.closeQuietly;
 
 public class HostConnector<T extends ClusteredNetworkContext<T>, C extends ClusterContext<C, T>> implements Closeable {
-
-    interface ClosableRunnable extends  Runnable, net.openhft.chronicle.core.io.Closeable {
-
-    }
-
     @NotNull
     private final ConnectionManager<T> connectionManager;
 
@@ -120,19 +114,7 @@ public class HostConnector<T extends ClusteredNetworkContext<T>, C extends Clust
                 .wireOutPublisher(wireOutPublisher)
                 .isAcceptor(false)
                 .heartbeatTimeoutMs(clusterContext.heartbeatTimeoutMs() * 2)
-                .socketReconnector(new ClosableRunnable(){
-                    @Override
-                    public void run() {
-                        close();
-                        if (!eventLoop.isClosing())
-                            connect();
-                    }
-
-                    @Override
-                    public void close() {
-                         closeQuietly(HostConnector.this.close());
-                    }
-                })
+                .socketReconnector(this::reconnect)
                 .serverThreadingStrategy(clusterContext.serverThreadingStrategy())
                 .wireType(this.wireType);
 
@@ -155,4 +137,9 @@ public class HostConnector<T extends ClusteredNetworkContext<T>, C extends Clust
         remoteConnector.connect(connectUri, eventLoop, nc, clusterContext.retryInterval());
     }
 
+    synchronized void reconnect() {
+        close();
+        if (!eventLoop.isClosing())
+            connect();
+    }
 }
