45d5a2308208ce9e80facd3384205c346834e7b7
Rob Austin <rob.austin@boundedbuffer.co.uk>
2021-06-29 13:47:03 +0100

    removed close from VanillaClientConnectionMonitor

--- src/main/java/net/openhft/chronicle/network/VanillaClientConnectionMonitor.java
+++ src/main/java/net/openhft/chronicle/network/VanillaClientConnectionMonitor.java
@@ -1,6 +1,5 @@
 package net.openhft.chronicle.network;
 
-import net.openhft.chronicle.core.io.AbstractCloseable;
 import net.openhft.chronicle.network.connection.ClientConnectionMonitor;
 import org.jetbrains.annotations.NotNull;
 
@@ -8,21 +7,16 @@ import java.net.SocketAddress;
 
 import static net.openhft.chronicle.core.Jvm.debug;
 
-public class VanillaClientConnectionMonitor extends AbstractCloseable implements ClientConnectionMonitor {
+public class VanillaClientConnectionMonitor implements ClientConnectionMonitor {
 
     @Override
     public void onConnected(String name, @NotNull SocketAddress socketAddress) {
-        throwExceptionIfClosed();
         debug().on(this.getClass(), "onConnected name=" + name + ",socketAddress=" + socketAddress);
     }
 
     @Override
     public void onDisconnected(String name, @NotNull SocketAddress socketAddress) {
-        throwExceptionIfClosed();
         debug().on(this.getClass(), "onDisconnected name=" + name + ",socketAddress=" + socketAddress);
     }
 
-    @Override
-    protected void performClose() throws IllegalStateException {
-    }
 }
