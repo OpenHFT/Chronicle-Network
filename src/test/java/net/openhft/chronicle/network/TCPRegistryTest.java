faf872a8ea09288383ff9bbb00872c453078b25a
Tom <tom.dellmann@chronicle.software>
2023-07-24 07:29:28 +0100

    Add unit test coverage fixes #262 (#263)
    Increase test coverage of TCPRegistry

--- src/test/java/net/openhft/chronicle/network/TCPRegistryTest.java
+++ src/test/java/net/openhft/chronicle/network/TCPRegistryTest.java
@@ -22,11 +22,17 @@ package net.openhft.chronicle.network;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class TCPRegistryTest extends NetworkTestCommon {
+
+    /**
+     * {@link TCPRegistry#lookup(String)} contains logic to look up an address from system properties where the key
+     * maps to a host:port pair. This key constant is used to map to various host:port combinations in tests below.
+     */
+    public static final String SYSTEM_PROPERTY_HOST_DETAILS_KEY = "exampleHostKey";
 
     @Test
     void testResetClearsRegistry() throws IOException {
@@ -45,6 +51,85 @@ public abstract class TCPRegistryTest extends NetworkTestCommon {
         TCPRegistry.createServerSocketChannelFor("host1", "host2", "host3");
         TCPRegistry.reset();
         TCPRegistry.reset();
+    }
+
+    @Test
+    void testAllServersStopped() throws IOException {
+        TCPRegistry.createServerSocketChannelFor("host1", "host2", "host3");
+        TCPRegistry.reset();
+        TCPRegistry.assertAllServersStopped();
+    }
+
+    @Test
+    void createServerSocketChannelFor_withHostAndInvalidPort() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> TCPRegistry.createServerSocketChannelFor("host1:-1")
+        );
+        assertEquals("port out of range:-1", exception.getMessage());
+    }
+
+    @Test
+    public void lookup_lookupViaSystemProperty_empty() {
+        try {
+            System.setProperty(SYSTEM_PROPERTY_HOST_DETAILS_KEY, "");
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> TCPRegistry.lookup(SYSTEM_PROPERTY_HOST_DETAILS_KEY));
+            assertEquals("Alias " + SYSTEM_PROPERTY_HOST_DETAILS_KEY + " as  malformed, expected hostname:port", exception.getMessage());
+        } finally {
+            System.clearProperty(SYSTEM_PROPERTY_HOST_DETAILS_KEY);
+        }
+    }
+
+    @Test
+    public void lookup_lookupViaSystemProperty_nullHostname() {
+        try {
+            System.setProperty(SYSTEM_PROPERTY_HOST_DETAILS_KEY, "null:");
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> TCPRegistry.lookup(SYSTEM_PROPERTY_HOST_DETAILS_KEY));
+            assertEquals("Invalid hostname \"null\"", exception.getMessage());
+        } finally {
+            System.clearProperty(SYSTEM_PROPERTY_HOST_DETAILS_KEY);
+        }
+    }
+
+    @Test
+    public void lookup_lookupViaSystemProperty_invalidPort() {
+        try {
+            System.setProperty(SYSTEM_PROPERTY_HOST_DETAILS_KEY, "a:z");
+            IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> TCPRegistry.lookup(SYSTEM_PROPERTY_HOST_DETAILS_KEY));
+            assertEquals("Alias " + SYSTEM_PROPERTY_HOST_DETAILS_KEY + " as a:z malformed, expected hostname:port with port as a number", exception.getMessage());
+        } finally {
+            System.clearProperty(SYSTEM_PROPERTY_HOST_DETAILS_KEY);
+        }
+    }
+
+    @Test
+    public void lookup_lookupViaSystemProperty_wellFormed() {
+        try {
+            System.setProperty(SYSTEM_PROPERTY_HOST_DETAILS_KEY, "host:9999");
+            InetSocketAddress address = TCPRegistry.lookup(SYSTEM_PROPERTY_HOST_DETAILS_KEY);
+            assertEquals("host", address.getHostName());
+            assertEquals(9999, address.getPort());
+        } finally {
+            System.clearProperty(SYSTEM_PROPERTY_HOST_DETAILS_KEY);
+        }
+    }
+
+    @Test
+    public void lookup_fallback_wellFormed() {
+        InetSocketAddress address = TCPRegistry.lookup("host:9999");
+        assertEquals("host", address.getHostName());
+        assertEquals(9999, address.getPort());
+    }
+
+    @Test
+    public void lookup_fallback_malformedPort() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> TCPRegistry.lookup("host:a"));
+        assertEquals("Description host:a malformed, expected hostname:port with port as a number", exception.getMessage());
+    }
+
+    @Test
+    void dumpAll() throws IOException {
+        TCPRegistry.createServerSocketChannelFor("host1", "host2", "host3");
+        TCPRegistry.dumpAllSocketChannels();
     }
 
     private void assertNotMapped(String hostName) {
